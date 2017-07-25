799245008d643e5ba7db255199b0bb3a2c832e0c
OlehMarko <olehmarko@gmail.com>
2017-07-25 16:57:41 +0300

    Update getTopNRichestNames.js

--- hw11/js/getTopNRichestNames.js
+++ hw11/js/getTopNRichestNames.js
@@ -27,10 +27,10 @@ function getTopNRichestNames(number, people) {
   }
 
 var people = [
-  {name: 'Bara', income: '13B'},
-  {name: 'Dara', income: '56B'},
-  {name: 'Kara', income: '16M'},
-  {name: 'Zara', income: '29K'}
+  {name: 'Bara', income: '1B'},
+  {name: 'Dara', income: '5B'},
+  {name: 'Kara', income: '1M'},
+  {name: 'Zara', income: '2K'}
  ];
 console.log(getTopNRichestNames(2, people));
 console.log(getTopNRichestNames(100, people)); 
