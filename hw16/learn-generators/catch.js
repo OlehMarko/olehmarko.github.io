4d4e1602b4d55079fdcf59d87bf03bd04ef95f56
OlehMarko <olehmarko@gmail.com>
2017-08-09 21:48:49 +0300

    Add files via upload

---
+++ hw16/learn-generators/catch.js
@@ -0,0 +1,16 @@
+function* upper(items) {
+  // your code goes here
+  for (let i = 0; i < items.length; i++) {
+    if (typeof items[i] === 'string') {
+      yield items[i].toUpperCase();
+    } else {
+      yield null;
+    }
+  }
+}
+
+var bad_items = ['a', 'B', 1, 'c'];
+
+for (var item of upper(bad_items)) {
+  console.log(item);
+}
---
+++ hw16/learn-generators/generator.js
@@ -0,0 +1,12 @@
+function* factorial(n) {
+ // your code here
+ let value = 1;
+ for (let i = 1; i <= n; i++) {
+   value *= i;
+   yield value;
+ }
+}
+
+for (var n of factorial(5)) {
+ console.log(n)
+}
